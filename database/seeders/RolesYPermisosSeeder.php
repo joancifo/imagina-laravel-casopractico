6f786a1e67ef098d3e6bb7f12453ae08eaeb63e6
Joan Cifo <joancifo@gmail.com>
2024-10-29 17:13:30 +0100

    feat: permisos

---
+++ database/seeders/RolesYPermisosSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesYPermisosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $alumnoBorrar = Permission::create(['name' => 'alumno:borrar']);
+
+        $alunmo = Role::create(['name' => 'Alumno']);
+        $editor = Role::create(['name' => 'Editor']);
+        $gestor =Role::create(['name' => 'Gestor']);
+
+//        $gestor->syncPermissions($alumnoBorrar->id);
+    }
+}
